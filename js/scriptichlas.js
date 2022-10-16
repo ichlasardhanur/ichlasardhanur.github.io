7bde13785272bd3d89964d13b065c4f02a5bd336
ichlasardhanur <113095236+ichlasardhanur@users.noreply.github.com>
2022-10-16 19:16:44 +0700

    update

--- js/scriptichlas.js
+++ js/scriptichlas.js
@@ -13,5 +13,25 @@ $(document).ready(function(){
         $("#isi_2").slideUp();
         $("#isi_3").slideUp();
         $("#isi_"+id_menu).slideToggle();
-    });
+
+
+      });
+});
+$(document).ready(function(){
+    $(".tugas").click(function(){
+        let id = $(this).prop('id');
+        let split = id.split('_');
+        let id_tugas = split[1];
+
+        $("tugas").removeClass('awal');
+        $("#tugas_"+id_tugas).addClass('awal');
+
+        $(".homet").slideUp();
+
+        $("#isit_1").slideUp();
+        $("#isit_2").slideUp();
+        $("#isit_3").slideUp();
+        $("#isit_4").slideUp();
+        $("#isit_"+id_tugas).slideToggle();
+      });
 });
